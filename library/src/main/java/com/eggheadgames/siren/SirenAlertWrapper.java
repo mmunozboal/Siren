5c7a9e6d83ef3d197861ab5745b945da46afb14c
mmunoz@toool.es <mmunoz@toool.es>
2018-05-10 10:11:45 +0200

    Version 1.5.3 sin no volver a mostrar

--- library/src/main/java/com/eggheadgames/siren/SirenAlertWrapper.java
+++ library/src/main/java/com/eggheadgames/siren/SirenAlertWrapper.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
